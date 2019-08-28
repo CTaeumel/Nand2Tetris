957cccce18f715fdcc0cfa4f3ce4af95e98cbd86
jonah iden <jonah.iden@nordakademie.com>
2019-08-28 16:18:27 +0200

    stuff

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@SCREEN
+D=A
+@ScreenRegIndex
+M=D
+
+(LOOP)    
+    // ist taste gedrückt spring zu FILLBLACK 
+    @KBD
+    D=M   
+    @FILLBLACK
+    D; JGT 
+    @FILLWHITE
+    D; JEQ
+    (FILLREGRETURN)
+    
+
+    // @ScreenRegIndex Increment
+    @ScreenRegIndex
+    M=M+1
+
+    // wenn counter = 24575 setze wieder auf SCREEN
+    @24575
+    D=A
+    @ScreenRegIndex //wenn screenregindex 
+    D=D-M
+    @RESETSCREENREG
+    D;JEQ
+    (RESETSCREENREGRETURN) // zurückspringpunkt für screenRegIndex
+
+    @LOOP // Sprung zurück zum start
+    M;JMP 
+
+
+(RESETSCREENREG) // Resesttet den ScreenRegIndex auf das erste Screen Register
+    @SCREEN
+    D=A
+    @ScreenRegIndex
+    M=D
+    @RESETSCREENREGRETURN
+    0;JMP 
+
+(FILLBLACK) // füllt ein register Schwarz
+    D=0
+    D=!D
+    @ScreenRegIndex
+    A=M
+    M=D
+    @FILLREGRETURN
+    0; JMP
+
+(FILLWHITE) // füllt ein register weiß
+    @ScreenRegIndex
+    M=-1
+    @FILLREGRETURN
+    0; JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,42 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@counter // counter auf null setzten
+M=0 
+@R2 // Ergebniss auf 0 setzten
+M=0
+@R0 // R0 = 0 stop
+D=M
+@INFLOOP
+D; JEQ
+@R1 // R1 = 0 stop
+D=M
+@INFLOOP
+D; JEQ
+(LOOP)
+    @R1 // load R0
+    D=M
+    @R2 //add with R1 into D
+    M=D+M // save D to R2
+    @counter // inc Counter
+    M=M+1
+    // if counter ungleich R0 Loop
+    D=M // load counter to D
+    @R0
+    A=M // load R0 to A
+    D=D-A // Conter - R0
+    @LOOP
+    D;JLT // if Counter - R0 
+
+(INFLOOP) // Go To Infnite Loop. End Of Prog
+    @INFLOOP
+    0;JMP
+
+
+
+
+
+
+
+
+
